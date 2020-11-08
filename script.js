83b08bd5ed16aa5f50dfae1e4b10d5fb8fc822c0
Radu Catalin <radu.catalin@gloobus.it>
2020-11-08 10:38:40 +0200

    Landing page mostly done

--- script.js
+++ script.js
@@ -14,11 +14,11 @@ $(document).ready(function() {
       autoplaySpeed: 1000,
       responsive:{
           0:{
-              items:3,
+              items:2,
               nav:true
           },
           600:{
-              items:4,
+              items:3,
               nav:true
           },
           1000:{
@@ -26,7 +26,41 @@ $(document).ready(function() {
               nav:true,
               loop:true,
               margin:30
+          },
+          1300:{
+            items:5,
+            nav:true,
+            margin:30
           }
       }
   })
+
+  //Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+
 });
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
+  function myFunction(x) {
+    x.classList.toggle("change");
+  }
+
+  $(".container-hamburger").click(function(){
+    $(".nav-links").toggle();
+  });
